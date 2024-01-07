c8df22a02ee8e86fb9b504322c94552f685784fe
pych1971 <pych1971@gmail.com>
2024-01-07 17:11:04 +0500

    070120241710

--- people.py
+++ people.py
@@ -23,8 +23,31 @@ class Person():
         self.weight = kg
 
 
-man = Person('Олег', 52, 179)
+class Warrior(Person):
+    """Создаём класс воина"""
+
+    def __init__(self, name, age, height):
+        super().__init__(name, age, height)
+        self.rage = 100
+
+    def get_rage(self):
+        """Получение заряда ярости"""
+        print('Заряд ярости равен', str(self.weight))
+
+    def description_person(self):
+        """Переопределение метода родителя"""
+        description = self.name + ', ему ' + str(self.age) + ', его заряд ярости ' + str(self.rage)
+        # print('Нового человека зовут', description)
+        return description
+
+
+warrior = Warrior('Конан', 32, 200)
+# # warrior.update_weight(150)
+# warrior.description_person()
+# warrior.get_rage()
+print('Нового человека зовут ' + warrior.description_person())
+# man = Person('Олег', 52, 179)
 # man.description_person()
-# man.weight = 81
-man.update_weight(81)
-man.get_weight()
+# # man.weight = 81
+# # man.update_weight(81)
+# # man.get_weight()
